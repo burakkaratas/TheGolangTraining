05c2be5bf201cea43e4f9ba251f4716b54a48828
burakkaratas <bur.karatas@gmail.com>
2016-11-04 14:39:49 +0300

    mutex example added

---
+++ src/ch1/server3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+	"net/http"
+	"fmt"
+	"log"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", myHandler2)
+	log.Fatal(http.ListenAndServe("prog:8000", nil))
+}
+
+func myHandler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
