190ef14e3dceff8531aa733e6a653da499cd4cf6
burakkaratas <bur.karatas@gmail.com>
2016-11-04 14:30:50 +0300

    mutex example added

--- src/ch1/server2.go
+++ src/ch1/server2.go
@@ -1,2 +1,30 @@
 package main
 
+import (
+	"sync"
+	"net/http"
+	"fmt"
+	"log"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", myHandler)
+	http.HandleFunc("/counter", myCounter)
+	log.Fatal(http.ListenAndServe("prog:8000", nil))
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	count++
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func myCounter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Fprintf(w, "Count %d\n", count)
+}
