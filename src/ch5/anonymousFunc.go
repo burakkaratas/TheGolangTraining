16efe5adfd4e35340e0dc128df7aa6f9d392d3a7
burakkaratas <bur.karatas@gmail.com>
2016-11-07 20:17:01 +0300

    anonymous function sample

---
+++ src/ch5/anonymousFunc.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func squares() func() int {
+	var x int
+	return func() int{
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+
+	s := squares()
+	fmt.Println(s())
+	fmt.Println(s())
+	fmt.Println(s())
+	fmt.Println(f())
+}
