9ca90d91739272d8b3ab6d2440eced2fdb39db77
burakkaratas <bur.karatas@gmail.com>
2016-11-09 23:07:22 +0300

    interface

---
+++ src/ch7/xmlDecoding.go
@@ -0,0 +1,30 @@
+package main
+
+import "io"
+
+type Name struct {
+	Local string
+}
+type Attr struct {
+	Name  Name
+	Value string
+}
+type Token interface{}
+type StartElement struct {
+	Name Name
+	Attr []Attr
+}
+type EndElement struct {
+	Name Name
+}
+type CharData []byte
+type Comment []byte
+type Decoder struct {
+	/* */
+}
+
+func NewDecoder(io.Reader) *Decoder
+func (*Decoder) Token() (Token, error) {
+
+}
+
